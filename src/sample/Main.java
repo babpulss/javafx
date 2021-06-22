717e9c6575ce4a7f5391559ee4eed75c4d4c238a
babpulss <babpulss@naver.com>
2021-06-22 23:35:32 +0900

    javafx practice

---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/root.fxml"));
+
+        Scene scene = new Scene(root);
+
+
+        primaryStage.setTitle("AppMain");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/sample/fxml/Root.java
@@ -0,0 +1,25 @@
+package sample.fxml;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Root implements Initializable {
+    @FXML
+    private Label label;
+    @FXML
+    private Slider bar;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        bar.proper
+    }
+
+}
